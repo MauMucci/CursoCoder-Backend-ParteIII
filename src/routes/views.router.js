7dd73784508aaa9eeaaf1fd524a44ffce0f5d9b6
Mauro Mucci <mauromucci@hotmail.com>
2024-12-22 21:10:30 -0300

    Coder back parte 3 - finalizado

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { productModel } from '/app/src/Mongo/Models/product.model.js';
+import { productModel } from '../Mongo/Models/product.model.js';
 import passport from 'passport';
 
 
